598b33b74d1d25b2c0ca5fc119c296cdeadd4669
bare74 <82520963+bare74@users.noreply.github.com>
2022-08-29 16:26:48 +0200

    fixed api problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
-
+fetch("https://api.apispreadsheets.com/data/C5d0ksnzjBYTAIo2/", {
+  method: "POST",
+  body: JSON.stringify({
+    data: {
+      message: " ",
+      birth_date: " ",
+      maternity_date: " ",
+      discharged_date: " ",
+    },
+  }),
+}).then((res) => {
+  if (res.status === 201) {
+    // SUCCESS
+  } else {
+    // ERROR
+  }
+});
