dadae6ad0336df0a7d64e26fba0f565a35ed94ae
bare74 <82520963+bare74@users.noreply.github.com>
2022-08-30 21:08:38 +0200

    reset form

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function handleClick(event) {
+  // 👇️ if you are submitting a form (prevents page reload)
+  event.preventDefault();
+
+  $.ajax({
+    url: "https://api.apispreadsheets.com/data/SBz4WYKUyzMo5P6J/",
+    type: "post",
+    data: $("#myForm").serializeArray(),
+    success: function () {
+      alert("Data registrert");
+      document.getElementById("myForm").reset();
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert("OBS! ingen registrering. Forsøk igjen...");
+      document.getElementById("myForm").reset();
+    },
+  });
+});
